7e795c35447fab19450926fd02243da5d5ce76b4
krejcir.david <krejcir.david@D20-10>
2024-05-30 09:37:03 +0200

    DALSI

--- faktorial/Program.vb
+++ faktorial/Program.vb
@@ -1,11 +1,12 @@
 Imports System
 
 Module Module1
-        Sub Main()
+    Sub Main()
         Dim cislo As Integer
         Dim vysledek As Integer = 1
+
         Console.Write("Zadejte celé èíslo: ")
-        cislo = Console.ReadLine()
+        cislo = Val(Console.ReadLine())
 
         For x As Integer = 1 To cislo
             vysledek *= x
@@ -13,6 +14,5 @@ Module Module1
 
         Console.WriteLine("Faktoriál èísla " & cislo & " je " & vysledek)
         Console.ReadKey()
-        End Sub
-    End Module
-
+    End Sub
+End Module
