5d60779876d3a0005bcca067ebeab48f28d1935b
krejcir.david <krejcir.david@D20-10>
2024-05-30 09:41:34 +0200

    DALSI ZMENA

--- faktorial/Program.vb
+++ faktorial/Program.vb
@@ -5,14 +5,19 @@ Module Module1
         Dim cislo As Integer
         Dim vysledek As Integer = 1
 
-        Console.Write("Zadejte celé èíslo: ")
+        Console.Write("Zadejte celé èíslo (maximálnì 10): ")
         cislo = Val(Console.ReadLine())
 
-        For x As Integer = 1 To cislo
-            vysledek *= x
-        Next
+        If cislo > 10 Then
+            Console.WriteLine("Èíslo je pøíliš velké, faktoriál nelze vypoèítat.")
+        Else
+            For x As Integer = 1 To cislo
+                vysledek *= x
+            Next
 
-        Console.WriteLine("Faktoriál èísla " & cislo & " je " & vysledek)
+            Console.WriteLine("Faktoriál èísla " & cislo & " je " & vysledek)
+        End If
+
         Console.ReadKey()
     End Sub
 End Module
